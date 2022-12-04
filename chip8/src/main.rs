7247d718854a41485b372ca98dd34b7ed5cc4a18
Glitch <3555869-mrglitch@users.noreply.gitlab.com>
2022-12-04 17:37:49 -0500

    implemented game loading logic

--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -5,8 +5,16 @@ mod keypad;
 mod system;
 use system::System;
 
+//use eframe::{epi::App, run_native};
+
+struct Chip8;
+
+impl App for Chip8 {}
+
 fn main() {
     let mut system = System::new();
+
+    //run_native("chip8",, native_options);
 
     loop {
         system.cycle();
--- chip8/src/system.rs
+++ chip8/src/system.rs
@@ -10,16 +10,46 @@ pub struct System {
 }
 
 impl System {
-    pub fn new() -> System {
+    pub fn new(game: &str) {
+        let mut memory = [0; 4096];
+        let cpu = Cpu::new();
+
+        let mut f = File::open(game).expect("Unable to locate ROM");
+        let mut buffer = Vec::new();
+        f.read_to_end(&mut buffer).expect("Unable to read ROM data");
+
+        for i in 0..buffer.len() {
+            memory[i + cpu.program as usize] = buffer[i];
+        }
+
         System {
-            cpu: Cpu::new(),
-            memory: [0; 4096],
+            cpu: cpu,
+            memory: memory,
             keypad: Keypad::new(),
             display: Display::new(),
-        }
-    }
+        };
 
-    pub fn cycle(&mut self) {
-        //! TODO
+        pub static FONTS: [u8; 80] = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+        ];
+
+        for i in 0..80 {
+            memory[i] = FONTS[i];
+        }
     }
 }
